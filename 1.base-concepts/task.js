8715f4cb66c18890e04d7b7c2025ca2d72a86f64
Korolev E <nuuchcha@bk.ru>
2024-03-17 12:30:08 +0500

    added solution to tasks

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,27 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
-  return arr;
+  let d = b ** 2 - 4 * a * c;
+  if (d < 0){
+    return arr;
+  }
+  if (d === 0){
+    let x = -b / (2 * a);
+    arr.push(x);
+    return arr;
+  }
+  if (d > 0){
+    let x1 = (-b + Math.sqrt(d)) / (2 * a);
+    let x2 = (-b - Math.sqrt(d)) / (2 * a);
+    arr.push(x1, x2);
+    return arr;
+  }
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  let percentMonth = percent / 100 / 12;
+  let credit = amount - contribution;
+  let monthlyPayment = credit * (percentMonth + (percentMonth / (((1 + percentMonth) ** countMonths) - 1)));
+  let totalAmount = monthlyPayment * countMonths;
+  return +totalAmount.toFixed(2);
 }
